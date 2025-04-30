697a2ad2f9ba36437ef3d86aa8b8aadd7dd4f488
Paung <paungcuy15@gmail.com>
2025-04-30 18:55:02 +0700

    laravhell

---
+++ resources/js/pages/anjay.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { computed } from 'vue'
+import { Head, Link } from '@inertiajs/vue3';
+import { useAppearance } from '@/composables/useAppearance';
+import { Button } from '@/components/ui/button';
+import { usePage } from '@inertiajs/vue3'
+
+const page = usePage()
+const { appearance, changeAppearance } = useAppearance();
+
+const user = computed(() => page.props.auth.user)
+
+</script>
+
+<template>
+    <Head title="Anjay">
+        <link rel="preconnect" href="https://rsms.me/" />
+        <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+    </Head>
+
+    <h1 class="text-3xl font-bold ">Anjay</h1>
+    <h1 class="text-3xl font-bold ">halo, {{ user.name }}</h1>
+    <p>{{ appearance }}</p>
+
+    <div class="flex justify-betweeen">
+        <Button @click="changeAppearance('dark')">Dark</Button>
+        <Button @click="changeAppearance('light')">Light</Button>
+        <Button @click="$route('welcome')">welcome </Button>
+    </div>
+
+
+</template>
